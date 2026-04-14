445074d4f4cf25326e53b5e6963c4a328d617cb4
sidgabrielmoura <sidgabrielmoura40@gmail.com>
2026-04-14 16:15:34 -0300

    feat

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,8 @@ const nextConfig: NextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://www.xn--etxgesto-xza.com.br" },
+          // { key: "Access-Control-Allow-Origin", value: "https://xn--etxgesto-xza.com.br" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
         ],
